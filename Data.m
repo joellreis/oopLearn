1a1330fa667d0b87e9dab33d3f3a7f5edc24c268
Joel Laguardia <joellreis@gmail.com>
2018-07-05 16:09:47 -0300

    Data.m final version

--- Data.m
+++ Data.m
@@ -1,14 +1,22 @@
 classdef Data < handle
     %Data represents a simple object as a data container with only one property
+    
     properties (SetObservable = true)
         current_data
         selected_data = 'peaks'
     end
+    
     properties (SetAccess = private, Hidden = true)
         peaks %precomputed peaks data
         membrane %precomputed mebrane data
         sinc %precomputed sinc data
     end
+    
+    events
+        dataChanged % the exposed data has changed
+        selecterror % an error occurred
+    end
+    
     methods
         function obj = Data(varargin) %initialize the object
         obj.peaks = peaks(35);
@@ -29,5 +37,30 @@ classdef Data < handle
                     data = obj.sinc;
             end
         end
-    end
-end
+        function set.selected_data(obj, selection)
+            if ismember(selection, ['peaks' 'membrane' 'sinc'])
+                obj.selected_data = selection;
+                notify(obj, 'dataChanged'); %notify event that the selected data has changed
+            else
+                notify(obj, 'selecterror')  %notify that an error has occured
+                display('Selected data must be ''peaks'', ''membrane'' ir ''sinc''');
+            end
+        end
+    end %methods
+    
+end %object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
